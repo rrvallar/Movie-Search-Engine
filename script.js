78a18d88ca63b0c4ead7b7fe7b7fd41a285c5d2c
Will <103394466+OhDangBro@users.noreply.github.com>
2022-06-21 12:38:02 -0400

    added tagline under poster, added netflix and prime seach

--- script.js
+++ script.js
@@ -31,10 +31,41 @@ $(document).ready(function () {
         
         fetch("https://streaming-availability.p.rapidapi.com/get/basic?country=us&imdb_id=" + movieTag + "&output_language=en", options)
           .then(response => response.json())
-          .then(response => console.log(response))
+         
+          .then((response) => {
+            console.log(response)
+            $("#tagLine").text(response.tagline);
+            if (response.streamingInfo.netflix) {
+              $("#streamingNetflix").text(response.streamingInfo.netflix.us.link);
+            }
+            else if (response.streamingInfo.prime) {
+              $("#streamingPrime").text(response.streamingInfo.prime.us.link);
+            }
+            else if (response.streamingInfo.disney) {
+              $("#streamingDisney").text(response.streamingInfo.disney.us.link);
+            }
+            else if (response.streamingInfo.hulu) {
+              $("#streamingHulu").text(response.streamingInfo.hulu.us.link);
+            }
+            else if (response.streamingInfo.apple) {
+              $("#streamingApple").text(response.streamingInfo.apple.us.link);
+            }
+            
+            
+            
+          });
+          // .then(response => $("#streamingNetflix").text(response.streamingInfo.netflix.us.link))
+          // .then((response) => {
+          //   $("#tagLine").text(response.tagline);
+          // })
+          
+
+          
           
           
   });
+  
+
 });
 
 console.log(movieTag);
